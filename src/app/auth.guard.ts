154ac2a613dcf3806d1f2bf2d8df8c7c22231a46
Anastasia <fioB7@yandex.ru>
2023-05-15 16:28:47 +0500

    доработаны guards

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -4,7 +4,6 @@ import {AuthService} from "./services/auth/auth.service";
 import {TuiDialogService} from "@taiga-ui/core";
 import {PolymorpheusComponent} from "@tinkoff/ng-polymorpheus";
 import {LoginComponent} from "./login/login.component";
-import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -16,20 +15,26 @@ export class AuthGuard implements CanActivate {
   );
 
   constructor(private authService: AuthService,
-              private router: Router,
               private dialogs: TuiDialogService,
-              private readonly injector: Injector) {}
-
-  login(): void {
-    this.loginDialog.subscribe();
+              private readonly injector: Injector,
+              private router: Router) {
   }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    if (!window.localStorage["jwt"]) {
-      this.router.navigateByUrl('').then();
-      this.login();
-      return this.loginDialog;
-    }
-    return of(true);
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+
+    if (window.localStorage.getItem("jwt"))
+      return true;
+
+    this.router.navigateByUrl("");
+
+    this.loginDialog.subscribe(
+      value => {
+        if (value)
+          this.router.navigateByUrl(next.url[0].path);
+      });
+
+    return false;
   }
 }
