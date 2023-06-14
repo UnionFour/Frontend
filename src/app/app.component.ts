47fa3b5b903e8a9bf6acb19f8c7387555caadf3a
Anastasia <fioB7@yandex.ru>
2023-06-14 20:51:55 +0500

    сделано окно сообщения при оформлении заказа

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild, ViewContainerRef} from '@angular/core';
 import {OrderingService} from "./core/services/ordering.service";
+import { ComponentHostDirective } from './shared/directives/component-host.directive';
+import {MessagingService} from "./core/services/messaging.service";
+import {MessageModalComponent} from "./shared/message-modal/message-modal.component";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +10,29 @@ import {OrderingService} from "./core/services/ordering.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent{
+  @ViewChild(ComponentHostDirective, {static: true}) public messageModalHost!: ComponentHostDirective;
 
+  constructor(private readonly _messagingService: MessagingService) {
+    const context: AppComponent = this;
+    this._messagingService.openingMessage$.subscribe((isOpen: boolean): void => {
+      if (isOpen) {
+        context.renderMessageModal();
+        setTimeout((): void => {
+          context.clearMessageModal();
+        },context._messagingService.timeOut);
+      } else {
+        context.clearMessageModal();
+      }
+    });
+  }
+
+  public renderMessageModal(): void {
+    const containerRef: ViewContainerRef = this.messageModalHost.viewContainerRef;
+    containerRef.clear();
+    containerRef.createComponent<MessageModalComponent>(MessageModalComponent);
+  }
+
+  public clearMessageModal(): void {
+    this.messageModalHost.viewContainerRef.clear();
+  }
 }
